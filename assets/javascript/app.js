1cb2ddcd259d2448f53f83490c7e7a86913446a4
Candace Buchanan <Candace@Candaces-MacBook-Pro.local>
2018-04-09 22:39:52 -0400

    timer is working! Now need to work on comparing user value to correct answer to generate results.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,40 +27,91 @@ $(document).ready(function () {
 
 });
 
-    // click to start then display quesions
-    $("button").click(function () {
+    // click to start then display questions
+    $("#startbtn").click(function () {
         $('#welcomeScreen').hide();
         $('#questions').show();
         $('#startScreen').show();
         $('#timer').show();
-        countdown();
+        run();
+        correctAnswer = 0;
+        incorrectAnswer = 0;
+        unanswered = 0;
         
         //variables
         var answered;
         var unanswered;
-        var seconds;
         var incorrectAnswer;
         var correctAnswer;
+        var count = 60;
+        var intervalId;
 
-        //timer function
-        function countdown() {
-            seconds = 60;
-            $('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
-            answered = true;
-            //sets timer to go down
-            time = setInterval(showCountdown, 1000);
+        //timer function (interval-solved class solution)
+        //when button is clicked, they will trigger the stop or run
+        $("#submitAnswers").on("click", stop);
+        $("#timeLeft").on("click", run);
+
+        function run() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);   
         }
 
-        function showCountdown() {
-            seconds--;
-            $('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
-            if (seconds < 1) {
-                clearInterval(time);
-                answered = false;
-                answerPage();
+        function decrement() {
+            //decrease count by 1
+            count--;
+            //show timer in tag id
+            $("#timeLeft").html("<h2>" + count + "</h2>");
+
+            //once timer hits zero
+            if (count === 0) {
+                stop();
+
+                alert("Times Up!");
             }
         }
+        //clear intervalId
+        function stop() {
+            clearInterval(intervalId);
+        }
         //end of Timer function
+
+        //Score answers
+        function getScore(form) {
+            var answersAndObjects = new Array();
+            answersAndObjects[0] = ["Oh Mylanta!", form.question1];
+            answersAndObjects[1] = ["Jodie Sweetin", form.question2];
+            answersAndObjects[2] = ["D.J.", form.question3];
+            answersAndObjects[3] = ["Steve", form.question4];
+
+            var theScore = 0;
+
+            for (i = 0; i < answersAndObjects.length; i++) {
+                currQuestionObject = answersAndObjects[i][1];
+                for (j = 0; j < currQuestionObject.length; j++) {
+                    if (currQuestionObject[j].checked && currQuestionObject[j].value == answersAndObjects[i][0]) {
+                        theScore++;
+                        break;
+                    }
+
+                }
+            }
+
+            theScore = Math.round(theScore / answersAndObjects.length * 100);
+            form.percentage.value = theScore + "%";
+        }
+        getScore();
+        
+
+
+        // click submit to stop timer
+        $("#submitAnswers").click(function () {
+            $('#timer').hide();
+            $('#questions').hide();
+            $('#playerAnswers').show();
+            $('#results').show();
+            clearTimeout(time);
+        });
+
 });
 
  //end of Trivia Game
